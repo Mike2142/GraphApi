24e5879493d617fde86db15f1040ed5af9c77364
Michael Kolyadin <mkolyadin@bi-telco.com>
2022-09-20 16:42:40 +0300

    db connector

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+var connStr = "Server=tcp:192.168.0.102,1433; Database='master';User ID='sa'; Password='!QAZ2wsx';Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(connStr));
 
 var app = builder.Build();
 
