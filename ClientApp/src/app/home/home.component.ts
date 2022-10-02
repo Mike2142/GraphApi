80c13eb440159b44100f0f0f2cb111602283f9bc
Michael Kolyadin <mkolyadin@bi-telco.com>
2022-10-02 11:52:08 +0300

    spa

---
+++ ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  /*
+  cy = cytoscape({
+
+    container: document.getElementById('cy'), // container to render in
+  
+    elements: [ // list of graph elements to start with
+      { // node a
+        data: { id: 'a' }
+      },
+      { // node b
+        data: { id: 'b' }
+      },
+      { // edge ab
+        data: { id: 'ab', source: 'a', target: 'b' }
+      }
+    ],
+  
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+  
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle',
+          'curve-style': 'bezier'
+        }
+      }
+    ],
+  
+    layout: {
+      name: 'grid',
+      rows: 1
+    }
+  
+  });
+
+  */
+
+}
