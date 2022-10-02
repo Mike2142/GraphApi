cb7120a8c45c74cd571cd623fb2b388872ae9393
Michael Kolyadin <mkolyadin@bi-telco.com>
2022-10-02 12:32:31 +0300

    cytoscape

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,55 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+declare function cytoscape(object: any): any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
-  /*
-  cy = cytoscape({
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('cy') cyDiv!: ElementRef;
+  cy: any;
 
-    container: document.getElementById('cy'), // container to render in
-  
-    elements: [ // list of graph elements to start with
-      { // node a
-        data: { id: 'a' }
-      },
-      { // node b
-        data: { id: 'b' }
-      },
-      { // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
-    ],
-  
-    style: [ // the stylesheet for the graph
-      {
-        selector: 'node',
-        style: {
-          'background-color': '#666',
-          'label': 'data(id)'
+  ngAfterViewInit() {
+        
+    this.cy = cytoscape({
+      container: document.querySelector("#cy"), // container to render in
+      elements: [ // list of graph elements to start with
+        { // node a
+          data: { id: 'a' }
+        },
+        { // node b
+          data: { id: 'b' }
+        },
+        { // edge ab
+          data: { id: 'ab', source: 'a', target: 'b' }
         }
-      },
-  
-      {
-        selector: 'edge',
-        style: {
-          'width': 3,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
-          'target-arrow-shape': 'triangle',
-          'curve-style': 'bezier'
-        }
-      }
-    ],
-  
-    layout: {
-      name: 'grid',
-      rows: 1
-    }
-  
-  });
+      ],
 
-  */
+      style: [ // the stylesheet for the graph
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+    
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier'
+          }
+        }
+      ],
+    
+      layout: {
+        name: 'grid',
+        rows: 1
+      }
+    
+    });
+
+  }
+
+
 
 }
